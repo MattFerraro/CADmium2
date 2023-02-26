0c9a308ed4ec46da2c8cb14cd1b3584b0f1e9c48
Matt Ferraro <mferraro89@gmail.com>
2023-02-25 23:34:32 -0500

    Add an electron build

---
+++ cadmium-spa/main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  console.log(`Hello from Electron 👋 Creating window`)
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+  })
+  win.loadFile('build/index.html')
+}
+
+// On Windows or Linux, a user closing all windows means kill everything
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.whenReady().then(() => {
+  createWindow()
+
+  // on mac, clicking the icon in the tray only opens a new window if there is not one already open
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
